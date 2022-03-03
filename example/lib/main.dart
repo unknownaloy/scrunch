2cd838a78ccf993ccd9c611286ee6bfd44a01dc9
unknownaloy <niceju4u@gmail.com>
2022-03-03 22:00:47 +0100

    Added example implementation in the main.dart file in the example directory. Added method into the scrunch library that checks the files passed in are image files before compression can take place

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
+import 'package:scrunch/scrunch.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +12,115 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ScrunchExamplePage(title: 'Scrunch Example Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ScrunchExamplePage extends StatefulWidget {
+  const ScrunchExamplePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ScrunchExamplePage> createState() => _ScrunchExamplePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ScrunchExamplePageState extends State<ScrunchExamplePage> {
+  final Scrunch _scrunch = Scrunch();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  File? _exampleFile;
+
+  File? _compressedFile;
+
+  bool _isCompressing = false;
+
+  void getImageFileFromAssets() async {
+    final byteData = await rootBundle.load('assets/example_image.jpg');
+
+    final file =
+        File('${(await getTemporaryDirectory()).path}/example_image.jpg');
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    setState(() => _exampleFile = file);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getImageFileFromAssets();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _exampleFile == null
+            ? const CircularProgressIndicator(
+                strokeWidth: 2.0,
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Uncompressed Image",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Image.asset("assets/example_image.jpg"),
+                    _compressedFile == null
+                        ? TextButton(
+                            onPressed: !_isCompressing
+                                ? () async {
+                                    setState(() => _isCompressing = true);
+                                    final result = await _scrunch
+                                        .compressImage([_exampleFile!], 6);
+
+                                    if (result != null) {
+                                      setState(() =>
+                                      _compressedFile = result[0]);
+                                    }
+                                  }
+                                : null,
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.orangeAccent)),
+                            child: Text(_isCompressing
+                                ? "Compressing..."
+                                : "Compress Image"),
+                          )
+                        : const SizedBox.shrink(),
+                    _compressedFile != null
+                        ? Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const SizedBox(
+                                height: 24.0,
+                              ),
+                              Text(
+                                "Compressed Image",
+                                style: Theme.of(context).textTheme.headline4,
+                              ),
+                              Image.file(_compressedFile!),
+                            ],
+                          )
+                        : const SizedBox.shrink(),
+                  ],
+                ),
+              ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/scrunch.dart
+++ lib/scrunch.dart
@@ -7,6 +7,7 @@ import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter_isolate/flutter_isolate.dart';
+import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart';
 
 /// Improvements that can be made include:
@@ -25,18 +26,32 @@ import 'package:path_provider/path_provider.dart';
 /// when it has completed its given task, but it is still advisable to call the
 /// [killIsolate] method in the off-chance that the isolate could still be running
 class Scrunch {
-  static const int _targetSize = (1024 * 1024) * 5;
-
   FlutterIsolate? _isolate;
   late ReceivePort _receivePort;
 
   /// This method will return a List of image files which has been compressed
   /// using a Isolate
-  Future<List<File>> compressImage(List<File?> files) async {
+  ///
+  /// NOTE: This method has a "target" parameter of type "int" which is an
+  /// optional parameter that defaults to "5" (i.e. 5 megabytes). This is the
+  /// size limit set for each of the image files
+  Future<List<File>?> compressImage(List<File?> files,
+      [int targetSize = 5]) async {
+
+    final filesToCompress = _extractOnlyImageFiles(files);
+
+    if (filesToCompress.isEmpty) {
+      return null; // None of the files passed in are image files
+    }
+
+    final compressFileSizeLimit = (1024 * 1024) * targetSize;
+
     _receivePort = ReceivePort();
 
     _isolate = await FlutterIsolate.spawn(
-        _imageCompressHandler, _receivePort.sendPort);
+      _imageCompressHandler,
+      _receivePort.sendPort,
+    );
 
     SendPort sendPort = await _receivePort.first;
 
@@ -47,7 +62,8 @@ class Scrunch {
     List<String> filePaths = _convertFilesToStringPaths(files);
 
     sendPort.send([
-      filePaths, // Sending the file path
+      filePaths, // <- Sending the file path
+      compressFileSizeLimit, // <- File compress size limit
       answerPort.sendPort,
     ]);
 
@@ -66,7 +82,6 @@ class Scrunch {
 
   /// This method will handle the compressing of the image file
   static Future<void> _imageCompressHandler(SendPort sendPort) async {
-
     /// Result to be return from method
     List<String> result = [];
 
@@ -78,18 +93,25 @@ class Scrunch {
 
     /// Extracting the passed data
     List<String> filePaths = msg[0] as List<String>;
-    SendPort replyPort = msg[1] as SendPort;
+    int fileCompressSizeLimit = msg[1] as int;
+    SendPort replyPort = msg[2] as SendPort;
 
     /// Get the list of files
     List<File> files = _convertStringPathsToFiles(filePaths);
 
     for (File file in files) {
-      bool shouldCompress = await _shouldImageBeCompressed(file);
+      bool shouldCompress = await _shouldImageBeCompressed(
+        file,
+        fileCompressSizeLimit,
+      );
 
       if (!shouldCompress) {
         result.add(file.path);
       } else {
-        int percentageToCompressWith = await _calculateCompressPercentage(file);
+        int percentageToCompressWith = await _calculateCompressPercentage(
+          file,
+          fileCompressSizeLimit,
+        );
         String originalPath = file.path;
         String compressPath = await _getCompressPath(file);
 
@@ -107,10 +129,6 @@ class Scrunch {
         );
 
         if (compressedFile != null) {
-          // int newImageSize = await _getImageSize(compressedFile);
-          //
-          // debugPrint("New Image Size: $newImageSize");
-
           result.add(compressedFile.path);
         } else {
           result.add(file.path);
@@ -126,11 +144,14 @@ class Scrunch {
   /// NOTE: This method uses the [_targetSize] value to determine if the
   /// image should be compressed. If the image size is greater than the
   /// [_targetSize], the method will return "true" and the image compressed
-  static Future<bool> _shouldImageBeCompressed(File file) async {
+  static Future<bool> _shouldImageBeCompressed(
+    File file,
+    int fileSizeLimit,
+  ) async {
     bool shouldBeCompressed = false;
     int imageSize = await _getImageSize(file);
 
-    if (imageSize > _targetSize) {
+    if (imageSize > fileSizeLimit) {
       shouldBeCompressed = true;
     }
 
@@ -174,11 +195,14 @@ class Scrunch {
 
   /// This method calculates the percentage the [FlutterImageCompress]
   /// package should use in compressing the image file
-  static Future<int> _calculateCompressPercentage(File file) async {
+  static Future<int> _calculateCompressPercentage(
+    File file,
+    int targetSize,
+  ) async {
     int imageSize = await _getImageSize(file); // Size of image
 
     /// Percentage image will be compressed with
-    double percentage = (_targetSize / imageSize) * 100;
+    double percentage = (targetSize / imageSize) * 100;
 
     return percentage.toInt();
   }
@@ -220,8 +244,6 @@ class Scrunch {
       int width = imageDimensions.width;
       int height = imageDimensions.height;
 
-      debugPrint("width -> $width == height -> $height");
-
       dimension = {
         "width": width,
         "height": height,
@@ -236,13 +258,40 @@ class Scrunch {
     return dimension;
   }
 
+  /// This method uses the "mime" to check if the file path passed points to
+  /// an image file
+  static bool _isImage(String path) {
+    final mimeType = lookupMimeType(path);
+
+    if (mimeType != null) {
+      return mimeType.startsWith('image/');
+    }
+
+    return false;
+  }
+
+  /// This method uses the [_isImage] method to extract and return the list of
+  /// files passed in that are images
+  ///
+  /// NOTE: The method return an empty list if all the files passed in are not
+  /// image files
+  static List<File> _extractOnlyImageFiles(List<File?> files) {
+    List<File> result = [];
+    for (File? file in files) {
+      if (file != null && _isImage(file.path)) {
+        result.add(file);
+      }
+    }
+
+    return result;
+  }
+
   /// This method terminates the running isolate if it's still active
   void killIsolate() {
     if (_isolate != null) {
       _receivePort.close();
       _isolate!.kill();
       _isolate = null;
-      debugPrint("Isolate terminated successfully");
     }
   }
 }
